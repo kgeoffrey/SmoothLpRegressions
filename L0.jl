20f0ff97f64577eacf57e5f3e4a77290f62f701e
Geoffrey Kasenbacher <gkasenbacher@gmail.com>
2019-12-04 18:16:46 +0100

    fixed bug

--- L0.jl
+++ L0.jl
@@ -1,6 +1,8 @@
 ### weird L0 regression
+using HigherOrderDerivatives
+using ForwardDiff
 
-function minimize_this(stepsize, epochs, w, X, Y)
+function descent(stepsize, epochs, w, X, Y)
     variance = var(X*w .- Y)
     closure = x -> G_loss(X, Y, x, variance)
     grad(x) = ForwardDiff.gradient(closure, x)
@@ -10,3 +12,44 @@ function minimize_this(stepsize, epochs, w, X, Y)
     end
     return w, variance
 end
+
+function L0_this(stepsize, epochs, X, Y)
+    w = rand(size(X,2))
+    loss = []
+    variances= []
+    for i in 1:epochs
+        mw, variance = minimize_this(stepsize, epochs/10, w, X, Y)
+        w = mw
+        append!(loss, G_loss(X, Y, w, variance))
+        append!(variances, variance)
+    end
+    return loss, w, variances
+end
+
+XX = rand(100, 5)*100
+YY = rand(100)*100
+ww = rand(5)
+
+
+G_loss(X, Y, w, vari) = length(Y) - sum(exp.(-(X*w .- Y).^2 ./ (2*vari)))
+closure = x -> G_loss(XX, YY, x, 0.5)
+
+fff(x) = exp(x'*x)
+
+derivative(fff, ww)
+
+
+
+
+
+
+
+ForwardDiff.gradient(closure, ww)
+
+t = w -> sum(-(XX*w .- YY).^2 )
+
+gradient(closure, ww, 1)
+
+t = Dual(ww)
+
+exp.((t'*t)).g
--- test.jl
+++ test.jl
@@ -28,14 +28,12 @@ f(x) = intercept .+ x'*w
 plot!(f, X, label="Least Squares")
 
 ###### L0 norm approximation ######
-
+## https://hal.archives-ouvertes.fr/hal-00173357/document
 
 
 G_loss(X, Y, w, vari) = length(Y) - sum(exp.(-(X*w - Y).^2 ./ (2*vari)))
 ww = rand(1)[1]
 G_loss(X,Y, ww, var(X*w -Y))
-
-using ForwardDiff
 
 closure = x -> G_loss(X, Y, x, vv)
 grad(x) = ForwardDiff.derivative(closure, x)
@@ -69,3 +67,45 @@ end
 lossL0, L0w, vars = L0_min(0.0001, 100)
 
 plot!(g, X, label = "L0 Loss")
+
+############ Multi Dimensional L0 Regression #############
+
+XX = rand(100, 5)*100
+YY = rand(100)*100
+ww = rand(5)
+
+G_loss(X, Y, w, vari) = length(Y) - sum(exp.(-(X*w - Y).^2 ./ (2*vari)))
+closure = x -> G_loss(XX, YY, x, vv)
+grad(x) = ForwardDiff.derivative(closure, x)
+
+function minimize_this(stepsize, epochs, w, X, Y)
+    variance = var(X*w .- Y)
+    closure = x -> G_loss(X, Y, x, variance)
+    grad(x) = ForwardDiff.gradient(closure, x)
+
+    for i in 1:epochs
+        w = w - stepsize*grad(w)
+    end
+    return w, variance
+end
+
+function L0_this(stepsize, epochs, X, Y)
+    w = rand(size(X,2))
+    loss = []
+    variances= []
+    for i in 1:epochs
+        mw, variance = minimize_this(stepsize, epochs/10, w, X, Y)
+        w = mw
+        append!(loss, G_loss(X, Y, w, variance))
+        append!(variances, variance)
+    end
+    return loss, w, variances
+end
+
+newloss, neww, sig2 = L0_this(0.0001, 100, XX, YY)
+
+plot(newloss)
+
+newloss
+
+plot(sig2)
