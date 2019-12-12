0e54ee71700110011821a0d47f18992f2bb18d08
Geoffrey Kasenbacher <gkasenbacher@gmail.com>
2019-12-12 12:30:16 +0100

    added plots

--- regressions.jl
+++ regressions.jl
@@ -108,37 +108,3 @@ function fit(::Type{L0}, X::AbstractArray, Y::AbstractArray, epochs::Int, stepsi
     end
     return M
 end
-
-
-
-
-function randdata(n)
-    # X = rand(1000)*1000
-    # Y = X .^(rand(Normal(1.,0.1), 1000)) .+ rand(Normal(0,1), 1000)
-    X = abs.(rand(n)*n)
-    Y = X .^((rand(Beta(50,1.8), n))) .+ (rand(Normal(1,50), n))
-    return X, Y
-end
-
-XX, YY = randdata(1000)
-scatter(XX, YY, markersize = 1)
-
-model = fit(Linf, XX, YY, 100, 0.0001)
-plot(model.loss)
-olsmodel = fit(OLS, XX, YY)
-l1model = fit(L1, XX, YY, 100, 0.000001)
-plot(l1model.loss)
-l0model = fit(L0, XX, YY, 100, 0.00001)
-plot(l0model.loss)
-
-
-f(x) = model.w[1] + x'*model.w[2]
-g(x) = olsmodel.w[1] + x'*olsmodel.w[2]
-h(x) = l1model.w[1] + x'*l1model.w[2]
-j(x) = l0model.w[1] + x'*l0model.w[2]
-
-scatter(XX, YY, markersize = 1)
-plot!(f, XX, linewidth = 3, label = "LINF")
-plot!(g, XX, linewidth = 3, label = "OLS")
-plot!(h, XX, linewidth = 3, label = "L1")
-plot!(j, XX, linewidth = 3, label = "L0")
